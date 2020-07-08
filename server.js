2f395f4325b9cfdf2ce6cb7499af38c4417b17d6
Ivan <trajchevivan@gmail.com>
2020-07-08 11:24:32 -0700

    Initial CICD commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
+const app = require('./server/app');
+const sequelize = require('./server/utils/db');
+
+const port = process.env.PORT || 3300;
+app.set('port', port);
+
+sequelize.sync()
+.then(result => {
+    // console.log(result);
+    app.listen(port);
+})
+.catch(err => {
+    console.log(err)
+});
