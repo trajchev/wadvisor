66e1afa20e33f80e8afe686cc01bc1a9c2b8eac3
Ivan <trajchevivan@gmail.com>
2020-07-09 23:23:40 -0700

    Severed connection to db for deployment purposes

--- server/app.js
+++ server/app.js
@@ -35,8 +35,7 @@ if (process.env.NODE_ENV === 'development') {
 // Limit requests from same IP
 const limiter = rateLimit({
     max: 100,
-    // windowMs: 60 * 60 * 1000,
-    windowMs: 60 * 60,
+    windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
