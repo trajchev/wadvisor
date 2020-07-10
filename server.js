a0291c8d1bf8cebe224d0a82a14801cc58ee6f59
Ivan <trajchevivan@gmail.com>
2020-07-09 23:44:10 -0700

    Edited files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 dotenv.config({path: './config.env'});
 
 const app = require('./server/app');
-// const sequelize = require('./server/utils/database');
+const sequelize = require('./server/utils/database');
 
 const port = process.env.PORT || 3300;
 app.set('port', port);
@@ -15,11 +15,11 @@ if (process.env.NODE_ENV === 'development') {
 
 app.listen(port);
 
-// sequelize.sync()
-// .then(result => {
-//     // console.log(result);
-//     app.listen(port);
-// })
-// .catch(err => {
-//     console.log(err)
-// });
+sequelize.sync()
+.then(result => {
+    // console.log(result);
+    app.listen(port);
+})
+.catch(err => {
+    console.log(err)
+});
