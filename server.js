633a80ff0f5224ca0544bcc1e865f54970e8f18b
Ivan <trajchevivan@gmail.com>
2020-07-09 23:37:28 -0700

    Retrieved previous state of files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 dotenv.config({path: './config.env'});
 
 const app = require('./server/app');
-const sequelize = require('./server/utils/database');
+// const sequelize = require('./server/utils/database');
 
 const port = process.env.PORT || 3300;
 app.set('port', port);
