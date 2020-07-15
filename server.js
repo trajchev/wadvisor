f4cecc6c0bd66b7f82840a96d0e0f7fd7b0ca50c
Ivan <trajchevivan@gmail.com>
2020-07-15 23:30:30 +0200

    Added base app

---
+++ server.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
+const app = require('./server/app');
+// const sequelize = require('./server/utils/db');
+
+const port = process.env.PORT || 3300;
+app.set('port', port);
+
+console.log(process.env.API_URL)
+
+app.listen(port);
+
+// sequelize.sync()
+// .then(result => {
+//     // console.log(result);
+//     app.listen(port);
+// })
+// .catch(err => {
+//     console.log(err)
+// });
