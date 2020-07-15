5bc02063b00a5f440ac94bfdbe5e4e15541b91a5
Ivan <trajchevivan@gmail.com>
2020-07-15 19:18:13 +0200

    Added mocks and data

--- tests/home.test.js
+++ tests/home.test.js
@@ -7,7 +7,8 @@ describe('Home page testing', () => {
 
   beforeEach(async () => {
     browser = await puppeteer.launch({
-        headless: false
+        headless: false,
+        args: ['--start-maximized']
     });
     // Launched the second tab
     page = await browser.newPage();
@@ -43,10 +44,11 @@ describe('Home page testing', () => {
       expect(url).toMatch(/auth/);
   });
 
-  test.only('When sign in, show user area', async () => {
+  test('When sign in, show user area', async () => {
     const id = 15;
     const jwt = require('jsonwebtoken');
     const token = jwt.sign({ id: id, exp: (Date.now() + process.env.JWT_EXPIRES_IN) / 1000 }, process.env.JWT_SECRET);
+
     await page.setCookie({name: 'jwt', value: token});
     await page.setExtraHTTPHeaders({'Authorization': `Bearer ${token}`});
     await page.goto('localhost:4200/me');
