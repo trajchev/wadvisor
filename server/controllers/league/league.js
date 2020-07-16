eed094cfe8309706cc46b116cef3eaab7ef9f7b5
Ivan <trajchevivan@gmail.com>
2020-07-16 16:37:28 +0200

    Clean slate

--- server/controllers/league/league.js
+++
@@ -1,8 +0,0 @@
-const { Group, Sport} = require('../../models');
-const { read } = require('../../services');
-
-const getLeagues = read.readAllSports(Group, Sport, 'sports');
-
-module.exports = {
-    getLeagues
-};
