97e6f62aaac8c27198f5ebb6e32827f57f9611ac
Ivan <trajchevivan@gmail.com>
2020-07-17 01:23:44 +0200

    Added routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,10 @@
+const user = require('./user');
+const league = require('./league');
+const match = require('./match');
+const site = require('./site');
+const team = require('./team');
 const faq = require('./faq');
+const ticket = require('./ticket');
+const page = require('./page');
 
-module.exports = {
-  faq
-}
+module.exports =  { user, league, match, site, page, team, faq, ticket };
