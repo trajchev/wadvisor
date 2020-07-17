ffa872af69548b61fed6a46556cdc93e8c8bd93f
Ivan <trajchevivan@gmail.com>
2020-07-17 13:10:22 +0200

    Added all routes to app.js

--- server/app.js
+++ server/app.js
@@ -1,17 +1,27 @@
 const path = require('path');
 const express = require('express');
 
-const { faq, user } = require('./routes');
+const { faq, user, league, match, site, page, team, ticket } = require('./routes');
 
 const app = express();
 
 // Routes
 app.use('/api/v1/users', user);
+app.use('/api/v1/leagues', league);
+app.use('/api/v1/matches', match);
+app.use('/api/v1/sites', site);
+app.use('/api/v1/pages', page);
+app.use('/api/v1/teams', team);
+app.use('/api/v1/tickets', ticket);
 app.use('/api/v1/faq', faq);
 
 app.use('/', express.static(path.join(__dirname, 'angular')));
 app.use('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'angular', 'index.html'))
+});
+
+app.all('*', (req, res, next) => {
+  next(new BAError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 module.exports = app
