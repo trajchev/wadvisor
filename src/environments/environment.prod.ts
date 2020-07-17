d27aeab0009ae98750be92b5617c749c73e92b66
Ivan <trajchevivan@gmail.com>
2020-07-17 14:23:18 +0200

    Edited endpoint string

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ENDPOINT_API: 'ec2-15-236-238-119.eu-west-3.compute.amazonaws.com/api/v1/',
+  ENDPOINT_API: 'http://waapp-env.eba-be47a3bq.eu-west-3.elasticbeanstalk.com/api/v1/',
   STRIPE_PK: 'pk_test_6pZQHOsdUEqn5WZ7zwOioXYe00XquhOFka',
   PRO_PRICE_ID: 'price_1GqdoWLKmNFcLwX9jShNGFzH'
 };
