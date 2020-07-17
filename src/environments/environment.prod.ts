d45b6254c70327c1c40d00d5a5b5142e281be41f
Ivan <trajchevivan@gmail.com>
2020-07-17 13:44:09 +0200

    Edited config FE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  ENDPOINT_API: 'ec2-15-236-238-119.eu-west-3.compute.amazonaws.com/api/v1/',
+  STRIPE_PK: 'pk_test_6pZQHOsdUEqn5WZ7zwOioXYe00XquhOFka',
+  PRO_PRICE_ID: 'price_1GqdoWLKmNFcLwX9jShNGFzH'
 };
