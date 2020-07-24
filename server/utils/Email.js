1555c2c3cba71594675192cbc97b33d0c9f0b3f2
Ivan <trajchevivan@gmail.com>
2020-07-24 14:13:23 +0200

    Edited the ssl config

--- server/utils/Email.js
+++ server/utils/Email.js
@@ -19,12 +19,6 @@ class Email {
         if (process.env.NODE_ENV === 'production') {
             // Sendgrid
             return nodemailer.createTransport({
-              // SES: new AWS.SES({
-              //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-              //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-              //   region: process.env.SMTP_REGION,
-              //   apiVersion: '2010-12-01'
-              // })
                 service: 'Sendgrid',
                 auth: {
                     user: process.env.SENDGRID_USERNAME,
@@ -34,13 +28,6 @@ class Email {
         }
 
         return nodemailer.createTransport({
-          // SES: new AWS.SES({
-          //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-          //   region: process.env.SMTP_REGION,
-          //   apiVersion: '2010-12-01'
-          // })
-
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
             auth: {
