4de98b08c19ff30c05fa9af7461e952b38560df0
Ivan <trajchevivan@gmail.com>
2020-08-04 01:37:12 +0200

    Fixed referal route bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,13 @@ import { AdminGuard } from './auth/admin.guard';
 
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./components/home/home.module').then(mod => mod.HomeModule), canActivate: [LoginGuard]},
+  { path: '', loadChildren: () => import('./components/home/home.module').then(mod => mod.HomeModule), canActivate: [LoginGuard]},
   { path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(mod => mod.AuthModule), canActivate: [LoginGuard]},
   { path: 'me', loadChildren: () => import('./components/user/user.module').then(mod => mod.UserModule), canActivate: [AuthGuard] },
   { path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(mod => mod.AdminModule), canActivate: [AuthGuard, AdminGuard] },
   { path: 'matches', loadChildren: () => import('./components/matches/matches.module').then(mod => mod.MatchesModule), canActivate: [AuthGuard, UnconfirmedGuard] },
   { path: 'tickets', loadChildren: () => import('./components/tickets/tickets.module').then(mod => mod.TicketsModule), canActivate: [AuthGuard, UnconfirmedGuard, ProGuard] },
-  { path: '**', loadChildren: () => import('./components/home/home.module').then(mod => mod.HomeModule), canActivate: [LoginGuard]}
+  { path: '**', redirectTo: 'me'}
 ];
 
 @NgModule({
