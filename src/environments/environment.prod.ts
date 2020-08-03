2800009ffdff4a285d1107ac204bdf96afe31ac2
Ivan <trajchevivan@gmail.com>
2020-08-03 11:46:58 +0200

    Edited api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ENDPOINT_API: 'http://bet-env.eba-c6mripni.eu-west-3.elasticbeanstalk.com/api/v1/',
+  ENDPOINT_API: 'https://win-advisor.com/api/v1/',
   STRIPE_PK: 'pk_test_6pZQHOsdUEqn5WZ7zwOioXYe00XquhOFka',
   PRO_PRICE_ID: 'price_1GqdoWLKmNFcLwX9jShNGFzH'
 };
