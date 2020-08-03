8dab96666ba7d1c3541d9c31569f8a6b6e15f072
Ivan <trajchevivan@gmail.com>
2020-08-03 12:31:38 +0200

    Edited email config

--- server/utils/Email.js
+++ server/utils/Email.js
@@ -15,25 +15,13 @@ class Email {
 
     newTransport() {
 
-        if (process.env.NODE_ENV === 'production') {
-            // Sendgrid
-            return nodemailer.createTransport({
-                service: 'Sendgrid',
-                auth: {
-                    user: process.env.SENDGRID_USERNAME,
-                    pass: process.env.SENDGRID_PASSWORD
-                }
-            });
+      return nodemailer.createTransport({
+        service: 'Sendgrid',
+        auth: {
+            user: process.env.SENDGRID_USERNAME,
+            pass: process.env.SENDGRID_PASSWORD
         }
-
-        return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
+      });
 
     }
 
