21e6688b1ff71bb143085242adf20c032e6ff4fc
Ivan <trajchevivan@gmail.com>
2020-11-24 19:47:27 +0100

    Added 2way slide

--- src/app/directives/horizontal-scroll.directive.ts
+++ src/app/directives/horizontal-scroll.directive.ts
@@ -6,12 +6,48 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class HorizontalScrollDirective {
 
   isDown: boolean = false;
+  isLeft: boolean = true;
   startX: any;
   scrollLeft: any;
+  private interval: any;
 
   constructor(
     private element: ElementRef
-  ) { }
+  ) {
+    this.slide();
+  }
+
+  slide() {
+    const time = 10;
+    this.interval = setInterval(() => {
+      const scroll = this.element.nativeElement.scrollLeft;
+      this.slideLeft(scroll);
+      if (scroll === this.element.nativeElement.scrollLeft) {
+        this.isLeft = false;
+      } else if (scroll === 0) {
+        this.isLeft = true;
+      }
+
+      if (this.isLeft) {
+        this.slideLeft(scroll)
+      } else {
+        this.slideRight(scroll)
+      }
+
+    }, time);
+  }
+
+  slideLeft(scroll) {
+    this.element.nativeElement.scrollLeft = scroll + 1;
+  }
+
+  slideRight(scroll) {
+    this.element.nativeElement.scrollLeft = scroll - 1;
+  }
+
+  @HostListener('mouseover') onHover() {
+    clearInterval(this.interval)
+  }
 
   @HostListener('mousedown', ['$event']) onMouseDown(event) {
 
@@ -24,6 +60,7 @@ export class HorizontalScrollDirective {
   @HostListener('mouseleave') mouseLeave() {
 
     this.isDown = false;
+    this.slide();
 
   }
 
